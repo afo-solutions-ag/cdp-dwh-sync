19e8a3db4777487a8c6a9fd88f50711c635fa479
rich-byte <kondla.dev@gmail.com>
2022-11-15 09:32:20 +0100

    remove SNSService from trigger hook providers

--- src/trigger-hook/trigger-hook.module.ts
+++ src/trigger-hook/trigger-hook.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { SNSModule } from '../sns/sns.module';
-import { SNSService } from '../sns/sns.sercive';
+import { SNSService } from '../sns/sns.service';
 import { GraphQLModule } from '../graphql/graphql.module';
 import { GraphQLService } from '../graphql/graphql.service';
 import { TriggerHookController } from './trigger-hook.controller';
@@ -9,13 +9,7 @@ import { TriggerHookService } from './trigger-hook.service';
 
 @Module({
   imports: [GraphQLModule, SNSModule],
-  providers: [
-    TriggerHookService,
-    SNSService,
-    GraphQLService,
-    ConfigService,
-    SNSModule,
-  ],
+  providers: [TriggerHookService, SNSService, GraphQLService, ConfigService],
   controllers: [TriggerHookController],
 })
 export class TriggerHookModule {}
